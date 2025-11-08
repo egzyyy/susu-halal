532f2d90c744d1ebe14b144f3214691548118746
jicoon1706 <jicoon1706@gmail.com>
2025-11-09 00:52:11 +0800

    sdfdsf

--- resources/views/labtech/labtech_process-milk.blade.php
+++ resources/views/labtech/labtech_process-milk.blade.php
@@ -49,9 +49,12 @@
       {{-- Data Table --}}
       <div class="data-table-container">
         <div class="table-header-info">
-          <span class="animals-selected">4 Animals Selected, 4 Data Lines</span>
+          <span class="animals-selected" id="first-count">4 Animals Selected, 4 Data Lines</span>
+          <button class="btn-add-row" onclick="addTableRow('first')">
+            <i class="fas fa-plus"></i> Add Row
+          </button>
         </div>
-        <table class="data-table">
+        <table class="data-table" id="first-table">
           <thead>
             <tr>
               <th>Ctrl No</th>
@@ -60,40 +63,61 @@
               <th>Fat</th>
               <th>Pro</th>
               <th>Scc</th>
+              <th>Actions</th>
             </tr>
           </thead>
           <tbody>
             <tr>
-              <td>2</td>
-              <td>ANDROME</td>
-              <td>2.4</td>
-              <td>5.81</td>
-              <td>4.78</td>
-              <td>48</td>
+              <td contenteditable="true">2</td>
+              <td contenteditable="true">ANDROME</td>
+              <td contenteditable="true">2.4</td>
+              <td contenteditable="true">5.81</td>
+              <td contenteditable="true">4.78</td>
+              <td contenteditable="true">48</td>
+              <td class="actions">
+                <button class="btn-delete-row" onclick="deleteTableRow(this)">
+                  <i class="fas fa-trash"></i>
+                </button>
+              </td>
             </tr>
             <tr>
-              <td>9</td>
-              <td>SIERRA</td>
-              <td>2.6</td>
-              <td>6.26</td>
-              <td>4.62</td>
-              <td>105</td>
+              <td contenteditable="true">9</td>
+              <td contenteditable="true">SIERRA</td>
+              <td contenteditable="true">2.6</td>
+              <td contenteditable="true">6.26</td>
+              <td contenteditable="true">4.62</td>
+              <td contenteditable="true">105</td>
+              <td class="actions">
+                <button class="btn-delete-row" onclick="deleteTableRow(this)">
+                  <i class="fas fa-trash"></i>
+                </button>
+              </td>
             </tr>
             <tr>
-              <td>10</td>
-              <td>RASPBRY</td>
-              <td>3.0</td>
-              <td>6.24</td>
-              <td>4.44</td>
-              <td>45</td>
+              <td contenteditable="true">10</td>
+              <td contenteditable="true">RASPBRY</td>
+              <td contenteditable="true">3.0</td>
+              <td contenteditable="true">6.24</td>
+              <td contenteditable="true">4.44</td>
+              <td contenteditable="true">45</td>
+              <td class="actions">
+                <button class="btn-delete-row" onclick="deleteTableRow(this)">
+                  <i class="fas fa-trash"></i>
+                </button>
+              </td>
             </tr>
             <tr>
-              <td>11</td>
-              <td>MAYPOP</td>
-              <td>3.0</td>
-              <td>6.18</td>
-              <td>4.03</td>
-              <td>82</td>
+              <td contenteditable="true">11</td>
+              <td contenteditable="true">MAYPOP</td>
+              <td contenteditable="true">3.0</td>
+              <td contenteditable="true">6.18</td>
+              <td contenteditable="true">4.03</td>
+              <td contenteditable="true">82</td>
+              <td class="actions">
+                <button class="btn-delete-row" onclick="deleteTableRow(this)">
+                  <i class="fas fa-trash"></i>
+                </button>
+              </td>
             </tr>
           </tbody>
         </table>
@@ -133,9 +157,12 @@
       {{-- Data Table --}}
       <div class="data-table-container">
         <div class="table-header-info">
-          <span class="animals-selected">4 Animals Selected, 4 Data Lines</span>
+          <span class="animals-selected" id="second-count">4 Animals Selected, 4 Data Lines</span>
+          <button class="btn-add-row" onclick="addTableRow('second')">
+            <i class="fas fa-plus"></i> Add Row
+          </button>
         </div>
-        <table class="data-table">
+        <table class="data-table" id="second-table">
           <thead>
             <tr>
               <th>Ctrl No</th>
@@ -144,40 +171,61 @@
               <th>Fat</th>
               <th>Pro</th>
               <th>Scc</th>
+              <th>Actions</th>
             </tr>
           </thead>
           <tbody>
             <tr>
-              <td>2</td>
-              <td>ANDROME</td>
-              <td>2.4</td>
-              <td>5.81</td>
-              <td>4.78</td>
-              <td>48</td>
+              <td contenteditable="true">2</td>
+              <td contenteditable="true">ANDROME</td>
+              <td contenteditable="true">2.4</td>
+              <td contenteditable="true">5.81</td>
+              <td contenteditable="true">4.78</td>
+              <td contenteditable="true">48</td>
+              <td class="actions">
+                <button class="btn-delete-row" onclick="deleteTableRow(this)">
+                  <i class="fas fa-trash"></i>
+                </button>
+              </td>
             </tr>
             <tr>
-              <td>9</td>
-              <td>SIERRA</td>
-              <td>2.6</td>
-              <td>6.26</td>
-              <td>4.62</td>
-              <td>105</td>
+              <td contenteditable="true">9</td>
+              <td contenteditable="true">SIERRA</td>
+              <td contenteditable="true">2.6</td>
+              <td contenteditable="true">6.26</td>
+              <td contenteditable="true">4.62</td>
+              <td contenteditable="true">105</td>
+              <td class="actions">
+                <button class="btn-delete-row" onclick="deleteTableRow(this)">
+                  <i class="fas fa-trash"></i>
+                </button>
+              </td>
             </tr>
             <tr>
-              <td>10</td>
-              <td>RASPBRY</td>
-              <td>3.0</td>
-              <td>6.24</td>
-              <td>4.44</td>
-              <td>45</td>
+              <td contenteditable="true">10</td>
+              <td contenteditable="true">RASPBRY</td>
+              <td contenteditable="true">3.0</td>
+              <td contenteditable="true">6.24</td>
+              <td contenteditable="true">4.44</td>
+              <td contenteditable="true">45</td>
+              <td class="actions">
+                <button class="btn-delete-row" onclick="deleteTableRow(this)">
+                  <i class="fas fa-trash"></i>
+                </button>
+              </td>
             </tr>
             <tr>
-              <td>11</td>
-              <td>MAYPOP</td>
-              <td>3.0</td>
-              <td>6.18</td>
-              <td>4.03</td>
-              <td>82</td>
+              <td contenteditable="true">11</td>
+              <td contenteditable="true">MAYPOP</td>
+              <td contenteditable="true">3.0</td>
+              <td contenteditable="true">6.18</td>
+              <td contenteditable="true">4.03</td>
+              <td contenteditable="true">82</td>
+              <td class="actions">
+                <button class="btn-delete-row" onclick="deleteTableRow(this)">
+                  <i class="fas fa-trash"></i>
+                </button>
+              </td>
             </tr>
           </tbody>
         </table>
@@ -217,9 +265,12 @@
       {{-- Data Table --}}
       <div class="data-table-container">
         <div class="table-header-info">
-          <span class="animals-selected">4 Animals Selected, 4 Data Lines</span>
+          <span class="animals-selected" id="third-count">4 Animals Selected, 4 Data Lines</span>
+          <button class="btn-add-row" onclick="addTableRow('third')">
+            <i class="fas fa-plus"></i> Add Row
+          </button>
         </div>
-        <table class="data-table">
+        <table class="data-table" id="third-table">
           <thead>
             <tr>
               <th>Ctrl No</th>
@@ -228,40 +279,61 @@
               <th>Fat</th>
               <th>Pro</th>
               <th>Scc</th>
+              <th>Actions</th>
             </tr>
           </thead>
           <tbody>
             <tr>
-              <td>2</td>
-              <td>ANDROME</td>
-              <td>2.4</td>
-              <td>5.81</td>
-              <td>4.78</td>
-              <td>48</td>
+              <td contenteditable="true">2</td>
+              <td contenteditable="true">ANDROME</td>
+              <td contenteditable="true">2.4</td>
+              <td contenteditable="true">5.81</td>
+              <td contenteditable="true">4.78</td>
+              <td contenteditable="true">48</td>
+              <td class="actions">
+                <button class="btn-delete-row" onclick="deleteTableRow(this)">
+                  <i class="fas fa-trash"></i>
+                </button>
+              </td>
             </tr>
             <tr>
-              <td>9</td>
-              <td>SIERRA</td>
-              <td>2.6</td>
-              <td>6.26</td>
-              <td>4.62</td>
-              <td>105</td>
+              <td contenteditable="true">9</td>
+              <td contenteditable="true">SIERRA</td>
+              <td contenteditable="true">2.6</td>
+              <td contenteditable="true">6.26</td>
+              <td contenteditable="true">4.62</td>
+              <td contenteditable="true">105</td>
+              <td class="actions">
+                <button class="btn-delete-row" onclick="deleteTableRow(this)">
+                  <i class="fas fa-trash"></i>
+                </button>
+              </td>
             </tr>
             <tr>
-              <td>10</td>
-              <td>RASPBRY</td>
-              <td>3.0</td>
-              <td>6.24</td>
-              <td>4.44</td>
-              <td>45</td>
+              <td contenteditable="true">10</td>
+              <td contenteditable="true">RASPBRY</td>
+              <td contenteditable="true">3.0</td>
+              <td contenteditable="true">6.24</td>
+              <td contenteditable="true">4.44</td>
+              <td contenteditable="true">45</td>
+              <td class="actions">
+                <button class="btn-delete-row" onclick="deleteTableRow(this)">
+                  <i class="fas fa-trash"></i>
+                </button>
+              </td>
             </tr>
             <tr>
-              <td>11</td>
-              <td>MAYPOP</td>
-              <td>3.0</td>
-              <td>6.18</td>
-              <td>4.03</td>
-              <td>82</td>
+              <td contenteditable="true">11</td>
+              <td contenteditable="true">MAYPOP</td>
+              <td contenteditable="true">3.0</td>
+              <td contenteditable="true">6.18</td>
+              <td contenteditable="true">4.03</td>
+              <td contenteditable="true">82</td>
+              <td class="actions">
+                <button class="btn-delete-row" onclick="deleteTableRow(this)">
+                  <i class="fas fa-trash"></i>
+                </button>
+              </td>
             </tr>
           </tbody>
         </table>
@@ -270,9 +342,8 @@
       <div class="button-row">
         <button class="btn-back-stage" onclick="switchStage('second')"><i class="fas fa-arrow-left"></i> Previous</button>
         <a href="{{ route('labtech.manage-milk-records') }}" class="btn-next">
-    Submit <i class="fas fa-check"></i>
-</a>
-
+          Submit <i class="fas fa-check"></i>
+        </a>
       </div>
     </div>
 
@@ -280,24 +351,19 @@
 </div>
 
 <script>
+// Switch between stages
 function switchStage(stage) {
-  // Hide all stage contents
   document.querySelectorAll('.stage-content').forEach(content => {
     content.classList.remove('active');
   });
   
-  // Remove active class from all tabs
   document.querySelectorAll('.stage-tab').forEach(tab => {
     tab.classList.remove('active');
   });
   
-  // Show selected stage
   document.getElementById(stage + '-stage').classList.add('active');
-  
-  // Activate corresponding tab
   document.querySelector(`[data-stage="${stage}"]`).classList.add('active');
   
-  // Scroll to top
   window.scrollTo({ top: 0, behavior: 'smooth' });
 }
 
@@ -306,6 +372,76 @@ document.querySelectorAll('.stage-tab').forEach(tab => {
   tab.addEventListener('click', function() {
     const stage = this.getAttribute('data-stage');
     switchStage(stage);
+  });
+});
+
+// Add new row to table
+function addTableRow(stage) {
+  const table = document.getElementById(stage + '-table').getElementsByTagName('tbody')[0];
+  const newRow = table.insertRow();
+  
+  // Add editable cells
+  for (let i = 0; i < 6; i++) {
+    const cell = newRow.insertCell(i);
+    cell.contentEditable = "true";
+    cell.textContent = "";
+    
+    // Add placeholder styling
+    if (i === 0) cell.textContent = "0";
+    else if (i === 1) cell.textContent = "COW NAME";
+    else cell.textContent = "0.0";
+  }
+  
+  // Add delete button cell
+  const actionsCell = newRow.insertCell(6);
+  actionsCell.className = "actions";
+  actionsCell.innerHTML = `
+    <button class="btn-delete-row" onclick="deleteTableRow(this)">
+      <i class="fas fa-trash"></i>
+    </button>
+  `;
+  
+  // Update count
+  updateRowCount(stage);
+  
+  // Focus on first cell
+  newRow.cells[0].focus();
+}
+
+// Delete row from table
+function deleteTableRow(button) {
+  const row = button.closest('tr');
+  const table = row.closest('table');
+  const stage = table.id.replace('-table', '');
+  
+  if (table.rows.length > 2) { // Keep at least header + 1 row
+    row.remove();
+    updateRowCount(stage);
+  } else {
+    alert('At least one data row is required.');
+  }
+}
+
+// Update row count display
+function updateRowCount(stage) {
+  const table = document.getElementById(stage + '-table');
+  const rowCount = table.getElementsByTagName('tbody')[0].rows.length;
+  const countElement = document.getElementById(stage + '-count');
+  countElement.textContent = `${rowCount} Animals Selected, ${rowCount} Data Lines`;
+}
+
+// Add enter key navigation for cells
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelectorAll('.data-table tbody td[contenteditable="true"]').forEach(cell => {
+    cell.addEventListener('keydown', function(e) {
+      if (e.key === 'Enter') {
+        e.preventDefault();
+        const nextCell = this.nextElementSibling;
+        if (nextCell && nextCell.contentEditable === "true") {
+          nextCell.focus();
+        }
+      }
+    });
   });
 });
 </script>
